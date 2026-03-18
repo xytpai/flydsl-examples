fba5cc676562f6bbd85b887623522f52cd144313
xytpai <xytpai@foxmail.com>
2026-03-18 15:24:38 +0000

    wmma ready

--- hgemm.py
+++ hgemm.py
@@ -11,7 +11,7 @@ import flydsl
 import flydsl.compiler as flyc
 import flydsl.expr as fx
 from flydsl.expr.typing import T
-from flydsl.expr import range_constexpr, arith, vector, gpu
+from flydsl.expr import range_constexpr, arith, vector, gpu, rocdl
 from flydsl._mlir import ir
 from flydsl.runtime.device import get_rocm_arch
 from flydsl.utils.smem_allocator import SmemAllocator, SmemPtr
@@ -69,6 +69,7 @@ def compile_hgemm_kernel(
     WARP_ATOM_K = 16
     WARP_SIZE = 64
     LDG_VEC_SIZE = 8
+    WMMA_FRAG_VALUES = 4 # WARP_ATOM_M * WARP_ATOM_K // WARP_SIZE
     # Propagated parameters:
     BLOCK_K_LOOPS = k // BLOCK_K
     WARP_K_STEPS = BLOCK_K // WARP_ATOM_K
@@ -100,6 +101,9 @@ def compile_hgemm_kernel(
         C: fx.Tensor,
     ):
         dtype_ = get_dtype_in_kernel(dtype)
+        mfma_fn = rocdl.mfma_f32_16x16x16f16
+        c_zero_f = arith.constant(0.0, type=T.f32)
+        acc_init = arith.constant_vector(0.0, T.f32x4)
 
         A_ = GTensor(A, dtype=dtype_, shape=(m, k))
         B_ = GTensor(B, dtype=dtype_, shape=(n, k))
@@ -111,6 +115,8 @@ def compile_hgemm_kernel(
         bs_ = STensor(smem_b_ptr, dtype_, shape=(STAGES, BLOCK_N, BLOCK_K))
         
         tid = fx.Int32(fx.thread_idx.x)
+        wid = tid // WARP_SIZE
+        w_tid = tid % WARP_SIZE
         block_m_idx = fx.block_idx.x
         block_n_idx = fx.block_idx.y
 
@@ -136,29 +142,65 @@ def compile_hgemm_kernel(
                     k_local_idx = global_tid % LDG_B_X_THREADS * LDG_VEC_SIZE
                     vec = B_.vec_load((b_offset + n_local_idx, k_offset + k_local_idx), LDG_VEC_SIZE)
                     bs_.vec_store((fx.Index(lds_stage), n_local_idx, k_local_idx), vec, LDG_VEC_SIZE)
-
-        c_zero_f = arith.constant(0.0, type=T.f32)
-
+        
+        c_frags = [acc_init] * (WARP_M_STEPS * WARP_N_STEPS)
+        warp_m_idx = wid // BLOCK_N_WARPS * WARP_M
+        warp_n_idx = wid % BLOCK_N_WARPS * WARP_N
+        ldmatrix_a_m_idx = w_tid % WARP_ATOM_M
+        ldmatrix_a_k_vec_idx = w_tid // WARP_ATOM_M * WMMA_FRAG_VALUES
+        ldmatrix_b_n_idx = w_tid % WARP_ATOM_N
+        ldmatrix_b_k_vec_idx = w_tid // WARP_ATOM_N * WMMA_FRAG_VALUES
+        def block_mma_sync(lds_stage):
+            s = fx.Index(lds_stage)
+            a_frags = [0] * (WARP_K_STEPS * WARP_M_STEPS)
+            b_frags = [0] * (WARP_K_STEPS * WARP_N_STEPS)
+            # load matrix a
+            for ii in range_constexpr(WARP_M_STEPS):
+                warp_atom_m_idx = warp_m_idx + ii * WARP_ATOM_M
+                for kk in range_constexpr(WARP_K_STEPS):
+                    warp_atom_k_idx = kk * WARP_ATOM_K
+                    vec = as_.vec_load((s, warp_atom_m_idx + ldmatrix_a_m_idx, warp_atom_k_idx + ldmatrix_a_k_vec_idx), WMMA_FRAG_VALUES)
+                    a_frags[kk * WARP_M_STEPS + ii] = vec
+            # load matrix b
+            for ii in range_constexpr(WARP_N_STEPS):
+                warp_atom_n_idx = warp_n_idx + ii * WARP_ATOM_N
+                for kk in range_constexpr(WARP_K_STEPS):
+                    warp_atom_k_idx = kk * WARP_ATOM_K
+                    vec = bs_.vec_load((s, warp_atom_n_idx + ldmatrix_b_n_idx, warp_atom_k_idx + ldmatrix_b_k_vec_idx), WMMA_FRAG_VALUES)
+                    b_frags[kk * WARP_N_STEPS + ii] = vec
+            # wmma
+            for ii in range_constexpr(WARP_M_STEPS):
+                warp_atom_m_idx = warp_m_idx + ii * WARP_ATOM_M
+                for jj in range_constexpr(WARP_N_STEPS):
+                    warp_atom_n_idx = warp_n_idx + jj * WARP_ATOM_N
+                    for kk in range_constexpr(WARP_K_STEPS):
+                        warp_atom_k_idx = kk * WARP_ATOM_K
+                        a_frag_vec = a_frags[kk * WARP_M_STEPS + ii]
+                        b_frag_vec = b_frags[kk * WARP_N_STEPS + jj]
+                        acc_in = c_frags[ii * WARP_N_STEPS + jj]
+                        c_frags[ii * WARP_N_STEPS + jj] = mfma_fn(T.f32x4, [a_frag_vec, b_frag_vec, acc_in, 0, 0, 0])
+        
         for bki in range_constexpr(BLOCK_K_LOOPS):
             ldg_a_copy(m_offset, k_offset, 0)
             ldg_b_copy(n_offset, k_offset, 0)
             gpu.barrier()
-            cond = arith.cmpi(arith.CmpIPredicate.eq, tid, fx.Int32(0))
-            if cond:
-                for mi in range(BLOCK_M):
-                    for ni in range(BLOCK_N):
-                        acc = c_zero_f
-                        for ki in range_constexpr(BLOCK_K):
-                            a_data = as_[fx.Index(0), mi, ki]
-                            a_data = a_data.extf(T.f32)
-                            b_data = bs_[fx.Index(0), ni, ki]
-                            b_data = b_data.extf(T.f32)
-                            acc = acc + a_data * b_data
-                        acc = acc.truncf(dtype_)
-                        old = C_[m_offset + mi, n_offset + ni]
-                        C_[m_offset + mi, n_offset + ni] = old + acc
+            block_mma_sync(0)
             k_offset += fx.Int32(BLOCK_K)
             gpu.barrier()
+        
+        # store results
+        stmatrix_c_m_vec_idx = w_tid // WARP_ATOM_N * WMMA_FRAG_VALUES
+        stmatrix_c_n_idx = w_tid % WARP_ATOM_N
+        for ii in range_constexpr(WARP_M_STEPS):
+            g_warp_atom_m_idx = m_offset + warp_m_idx + ii * WARP_ATOM_M
+            for jj in range_constexpr(WARP_N_STEPS):
+                g_warp_atom_n_idx = n_offset + warp_n_idx + jj * WARP_ATOM_N
+                out_vec = c_frags[ii * WARP_N_STEPS + jj]
+                for kk in range_constexpr(WMMA_FRAG_VALUES):
+                    out_m_idx = g_warp_atom_m_idx + stmatrix_c_m_vec_idx + kk
+                    out_n_idx = g_warp_atom_n_idx + stmatrix_c_n_idx
+                    val = vector.extract(out_vec, static_position=[kk], dynamic_position=[])
+                    C_[out_m_idx, out_n_idx] = val.truncf(dtype_)
         return
     
     @flyc.jit
@@ -178,6 +220,19 @@ def compile_hgemm_kernel(
         hgemm_kernel(A, B, C).launch(grid=(bm, bn, 1), block=(BLOCK_THREADS, 1, 1), stream=stream)
     
     return launch_hgemm_kernel
+
+
+# def shuffle_b(x, layout=(16, 16), k_steps=2):
+#     x_shape = x.shape
+#     VEC_SIZE = 16 // x.element_size()
+#     BN = layout[0]
+#     BK = layout[1] * k_steps
+#     assert x.shape[-2] % BN == 0, f"{x.shape[-2]} % {BN} == {x.shape[-2] % BN }"
+#     assert x.shape[-1] % BK == 0, f"{x.shape[-1]} % {BK} == {x.shape[-1] % BK }"
+#     x = x.view(-1, x.shape[-2] // BN, BN, x.shape[-1] // BK, BK // VEC_SIZE, VEC_SIZE)
+#     x = x.permute(0, 1, 3, 4, 2, 5).contiguous().view(*x_shape)
+#     x.is_shuffled = True
+#     return x
 
 
 def func(a, b, c):
