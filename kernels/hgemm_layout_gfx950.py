d451eb6ca107c7f7ebf9e5d9d627b0a08bc70509
xytpai <xytpai@foxmail.com>
2026-07-20 04:56:12 +0000

    refine sbarrier

--- kernels/hgemm_layout_gfx950.py
+++ kernels/hgemm_layout_gfx950.py
@@ -219,10 +219,6 @@ def __barrier(vmcnt=0):
     )
 
 
-def __s_barrier():
-    llvm.InlineAsmOp(None, [], "s_barrier", "", has_side_effects=True)
-
-
 def __waitcnt(vmcnt=0):
     llvm.InlineAsmOp(None, [], f"s_waitcnt vmcnt({vmcnt})", "", has_side_effects=True)
 
@@ -843,9 +839,9 @@ def hgemm_hti_gfx950_kernel(
     async_load_a_to_lds(1, 0, 0)
     rocdl.sched_barrier(0)
     if wid // n_waves == 1:
-        __s_barrier()
+        rocdl.s_barrier()
     rocdl.sched_barrier(0)
-    __s_barrier()
+    rocdl.s_barrier()
     rocdl.sched_barrier(0)
     async_load_b_to_lds(0, 1, 1)
     async_load_a_to_lds(0, 1, 1)
@@ -858,23 +854,23 @@ def hgemm_hti_gfx950_kernel(
         b0 = load_b_fragment(0, 0, k_tile)
         a0 = load_a_fragment(0, 0, k_tile)
         async_load_a_to_lds(1, k_tile + 1, 1)
-        __s_barrier()
+        rocdl.s_barrier()
         consume(k_tile, c00, a0, b0, True)
-        __s_barrier()
+        rocdl.s_barrier()
 
         b1 = load_b_fragment(1, 0, k_tile)
         if const_expr(prefetch_next):
             async_load_b_to_lds(0, next_k_tile, 0)
-            __s_barrier()
+            rocdl.s_barrier()
         consume(k_tile, c01, a0, b1, True)
-        __s_barrier()
+        rocdl.s_barrier()
 
         a1 = load_a_fragment(1, 0, k_tile)
         if const_expr(prefetch_next):
             async_load_a_to_lds(0, next_k_tile, 0)
-            __s_barrier()
+            rocdl.s_barrier()
         consume(k_tile, c10, a1, b0, True)
-        __s_barrier()
+        rocdl.s_barrier()
 
         b0 = load_b_fragment(0, 1, k_tile + 1)
         if const_expr(prefetch_next):
@@ -883,34 +879,34 @@ def hgemm_hti_gfx950_kernel(
         consume(k_tile, c11, a1, b1, True)
         if const_expr(not prefetch_next):
             __waitcnt(0)
-        __s_barrier()
+        rocdl.s_barrier()
 
         a0 = load_a_fragment(0, 1, k_tile + 1)
         if const_expr(prefetch_next):
             async_load_a_to_lds(1, next_k_tile, 0)
-            __s_barrier()
+            rocdl.s_barrier()
         consume(k_tile + 1, c00, a0, b0, True)
-        __s_barrier()
+        rocdl.s_barrier()
 
         b1 = load_b_fragment(1, 1, k_tile + 1)
         if const_expr(prefetch_next):
             async_load_b_to_lds(0, next_k_tile + 1, 1)
-            __s_barrier()
+            rocdl.s_barrier()
         consume(k_tile + 1, c01, a0, b1, True)
-        __s_barrier()
+        rocdl.s_barrier()
 
         a1 = load_a_fragment(1, 1, k_tile + 1)
         if const_expr(prefetch_next):
             async_load_a_to_lds(0, next_k_tile + 1, 1)
-            __s_barrier()
+            rocdl.s_barrier()
         consume(k_tile + 1, c10, a1, b0, True)
-        __s_barrier()
+        rocdl.s_barrier()
 
         if const_expr(prefetch_next):
             async_load_b_to_lds(1, next_k_tile + 1, 1)
             __barrier(1 * half_ldg_b_iters + 1 * half_ldg_a_iters)
         consume(k_tile + 1, c11, a1, b1, True)
-        __s_barrier()
+        rocdl.s_barrier()
 
     final_double_tile = ((k_tiles % 2) == 0).select(k_tiles - 2, k_tiles - 1)
     main_loop_end = (k_tiles > 2).select(final_double_tile, 0)
@@ -1036,10 +1032,10 @@ def hgemm(
         "block_n": 256,
         "block_k": 64,
         "stages": 2,
-        "m_waves": 4,
+        "m_waves": 2,
         "n_waves": 4,
         "group_m": 0,
-        "use_half_tile_interleaved": False,
+        "use_half_tile_interleaved": True,
     }
 
     kwargs.update(user_kwargs)
